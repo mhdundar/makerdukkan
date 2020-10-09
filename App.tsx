f8e02115845de5d9bdffca6c90f9f553f04e3478
Muhammet Dündar <mhdundar@outlook.com>
2020-10-09 23:22:31 +0300

    test

--- App.tsx
+++ App.tsx
@@ -1,55 +1,17 @@
-import React, { Component } from 'react'
-import {
-  StyleSheet,
-  TouchableOpacity,
-  Text,
-  View,
-  ListView,
-} from 'react-native'
-import list from './connection'
+import React from 'react';
+import { Text, View } from 'react-native';
+import data from './connection';
 
-class App extends Component {
-  state = {
-    count: 0
-  }
-
-  onPress = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-
- render() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity
-         style={styles.button}
-         onPress={this.onPress}
-        >
-         <Text>Click me</Text>
-        </TouchableOpacity>
-        <View>
-          <Text>
-            You clicked {  } times
-          </Text>
-        </View>
-      </View>
-    )
-  }
+const HelloWorldApp = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+      <Text></Text>
+    </View>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10,
-    marginBottom: 10
-  }
-})
-
-export default App;
+export default HelloWorldApp;
