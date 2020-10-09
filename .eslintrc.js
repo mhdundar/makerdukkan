f1643591e617ec7ab822b0536676e73fa2f3cc12
Ömer Güven <omr.gven@gmail.com>
2020-10-09 23:35:53 +0300

    connection update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,24 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: ["@react-native-community", "prettier"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "react-hooks/exhaustive-deps": 0,
+    "react-native/no-unused-styles": 1,
+    "react/jsx-key": 2,
+    eqeqeq: ["error", "smart"],
+    /*
+    "react/jsx-no-bind": [
+      1,
+      {
+        ignoreDOMComponents: false,
+        ignoreRefs: false,
+        allowArrowFunctions: true,
+        allowFunctions: false,
+        allowBind: false,
+      },
+    ],
+    */
+  },
 };
--- connection.js
+++ connection.js
@@ -1,49 +1,19 @@
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require("mongodb");
+
 const uri =
-  'mongodb+srv://dukkan:1@cluster0.zrwkq.mongodb.net/makerDb?retryWrites=true&w=majority';
+  "mongodb+srv://dukkan:1@cluster0.zrwkq.mongodb.net/makerDb?retryWrites=true&w=majority";
+
 const client = new MongoClient(uri);
 
-async function main() {
-  const uri =
-    'mongodb+srv://dukkan:1@cluster0.zrwkq.mongodb.net/makerDb?retryWrites=true&w=majority';
-
-  
-
-  try {
-
-    await client.connect();
-    await createListing(client, {
-      name: 'Lovely Loft',
-      summary: 'A charming loft in Paris',
+try {
+  client.connect().then(() => {
+    client.db("sample_airbnb").collection("listingsAndReviews").insertOne({
+      name: "Lovely Loft",
+      summary: "A charming loft in Paris",
       bedrooms: 1,
       bathrooms: 1,
     });
-  } finally {
-    await client.close();
-  }
-}
-
-main().catch(console.error);
-
-
-async function createListing(client, newListing) {
-  const result = await client
-    .db('sample_airbnb')
-    .collection('listingsAndReviews')
-    .insertOne(newListing);
-  console.log(
-    `New listing created with the following id: ${result.insertedId}`,
-  );
-}
-
-async function createMultipleListings(client, newListings) {
-  const result = await client
-    .db('sample_airbnb')
-    .collection('listingsAndReviews')
-    .insertMany(newListings);
-
-  console.log(
-    `${result.insertedCount} new listing(s) created with the following id(s):`,
-  );
-  console.log(result.insertedIds);
+  });
+} finally {
+  client.close();
 }
